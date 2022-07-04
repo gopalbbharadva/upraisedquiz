e8bee38e57826c270fc49cd066ba2a3189eda6dd
gopal <gopalbbharadva@gmail.com>
2022-07-04 14:36:11 +0530

    Provider: Context providers added at root

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QuizContextProvider } from "./Context/QuizContext";
+import { DataContextProvider } from "./Context/DataContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <DataContextProvider>
+        <QuizContextProvider>
+          <App />
+        </QuizContextProvider>
+      </DataContextProvider>
+    </Router>
   </React.StrictMode>
 );
 
